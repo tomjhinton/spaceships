18a782a935a02ccc4c7c4b9049c1c54b259e04ce
Tom Hinton <tomjhinton@gmail.com>
2019-11-22 15:59:34 +0000

    move ship around the screen

--- src/app.js
+++ src/app.js
@@ -5,11 +5,45 @@ import './style.scss'
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+function keyDown(/** @type {KeyboardEvent} */ ev) {
+            switch(ev.keyCode) {
+                case 37: // left arrow (rotate ship left)
+                    player.rotation = player.thrustSpeed / 180 * Math.PI
+                    break;
+                case 38: // up arrow (thrust the player forward)
+                    player.thrusting = true;
+                    break;
+                case 39: // right arrow (rotationate player right)
+                    player.rotation = -player.thrustSpeed / 180 * Math.PI
+                    break;
+            }
+        }
+
+        function keyUp(/** @type {KeyboardEvent} */ ev) {
+            switch(ev.keyCode) {
+                case 37: // left arrow (stop rotationating left)
+                    player.rotation = 0;
+                    break;
+                case 38: // up arrow (stop thrusting)
+                    player.thrusting = false;
+                    break;
+                case 39: // right arrow (stop rotationating right)
+                    player.rotation = 0;
+                    break;
+            }
+        }
 
 ctx.clearRect(0, 0, canvas.width, canvas.height)
 ctx.fillStyle = 'black'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+
+const world = {
+  friction: 0.02
+}
 
 const player = {
   height: 50,
@@ -21,7 +55,9 @@ const player = {
   speed: 3,
   angle: 90 / 180 * Math.PI,
   size: 30,
-  rotation: 10,
+  rotation: 0,
+  thrustSpeed: 5,
+  turnSpeed: 360,
   thrusting: false,
   thrust: {
     x: 0,
@@ -32,9 +68,11 @@ const player = {
 }
 
 function  update(){
-
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
   // Draw player triangle
-  ctx.strokeStyle = `rgba(255,255,255,0.2)`
+  ctx.strokeStyle = `rgba(255,255,255,1)`
   ctx.lineWidth = 1
 
   ctx.beginPath()
@@ -56,8 +94,22 @@ function  update(){
 
   player.angle += player.rotation
 
-  player.posX+=1
-  player.size+=1
+  if (player.thrusting) {
+    player.thrust.x += player.thrustSpeed * Math.cos(player.angle)
+    player.thrust.y -= player.thrustSpeed * Math.sin(player.angle)
+
+
+  }else {
+  // apply friction (slow the ship down when not thrusting)
+    player.thrust.x -= world.friction * player.thrust.x
+    player.thrust.y -= world.friction * player.thrust.y
+  }
+
+
+  
+
+  player.posX += player.thrust.x
+  player.posY += player.thrust.y
 
 }
 
