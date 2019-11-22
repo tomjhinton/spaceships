95db468bc2ea8058d0ae6a5645dcb3bee74a6c43
Tom Hinton <tomjhinton@gmail.com>
2019-11-22 15:19:06 +0000

    ship angle setup

--- src/app.js
+++ src/app.js
@@ -14,35 +14,52 @@ ctx.fillRect(0, 0, canvas.width, canvas.height)
 const player = {
   height: 50,
   width: 20,
-  posX: 75,
-  posY: 50,
+  posX: 275,
+  posY: 250,
   velX: 0,
   velY: 0,
   speed: 3,
-  jumping: false,
-  grounded: false
+  angle: 90 / 180 * Math.PI,
+  size: 30,
+  rotation: 10,
+  thrusting: false,
+  thrust: {
+    x: 0,
+    y: 0
+  }
+
 
 }
 
 function  update(){
 
   // Draw player triangle
-  ctx.strokeStyle = 'white'
-  ctx.lineWidth = 3
+  ctx.strokeStyle = `rgba(255,255,255,0.2)`
+  ctx.lineWidth = 1
+
   ctx.beginPath()
-  ctx.moveTo(player.posX, player.posY)
-  ctx.lineTo(player.posX+25, player.posY+25)
-  ctx.lineTo(player.posX+25, player.posY-25)
-  ctx.lineTo(player.posX, player.posY)
+  ctx.moveTo( // nose of the ship
+    player.posX + 4 / 3 * player.size * Math.cos(player.angle),
+    player.posY - 4 / 3 * player.size * Math.sin(player.angle)
+  )
+  ctx.lineTo( // rear left
+    player.posX - player.size * (2 / 3 * Math.cos(player.angle) + Math.sin(player.angle)),
+    player.posY + player.size * (2 / 3 * Math.sin(player.angle) - Math.cos(player.angle))
+  )
+  ctx.lineTo( // rear right
+    player.posX - player.size * (2 / 3 * Math.cos(player.angle) - Math.sin(player.angle)),
+    player.posY + player.size * (2 / 3 * Math.sin(player.angle) + Math.cos(player.angle))
+  )
+  ctx.closePath()
   ctx.stroke()
 
 
+  player.angle += player.rotation
 
+  player.posX+=1
+  player.size+=1
 
-
-
-  
-  requestAnimationFrame(update())
 }
 
-update()
+
+setInterval(update,  100)
